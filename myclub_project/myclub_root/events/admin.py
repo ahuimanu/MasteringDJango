78f92ebbb53fdbeaba616976c1c034054c4c4ee9
Jeffry Babb <ahuimanu@gmail.com>
2021-01-09 16:16:02 -0600

    updated requirements and admin

--- myclub_project/myclub_root/events/admin.py
+++ myclub_project/myclub_root/events/admin.py
@@ -16,7 +16,7 @@ class EventAdmin(admin.ModelAdmin):
     fields = (("name", "venue"), "event_date", "description", "manager")
     list_display = ("name", "event_date", "venue")
     list_filter = ("event_date", "venue")
-    ordering = "-event_date"
+    ordering = ("-event_date",)
 
 
 # admin.site.register(Event)
